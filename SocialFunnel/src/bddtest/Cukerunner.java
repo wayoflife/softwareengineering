d6a791a0d4b4293e299d6abc9c4c7d41e64349fe
wayoflife <dbetsche@gmail.com>
2014-10-30 15:53:26 +0100

    Cucumber correct install

--- SocialFunnel/src/bddtest/Cukerunner.java
+++ SocialFunnel/src/bddtest/Cukerunner.java
@@ -7,21 +7,12 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(	features = { "src/" },
+					format = { "pretty",
+					"html:target/cucumber-html-report",
+					"junit:target/cucumber-junit-report/allcukes.xml" },
+					tags = { "@Runme" })
 
-@CucumberOptions(
-
-features = {“src/”},
-
-format = {“pretty”,
-
-“html:target/cucumber-html-report”,
-
-“junit:target/cucumber-junit-report/allcukes.xml”},
-
-tags = {“@Runme”}
-
-)
-
-public class cukerunner {
+public class Cukerunner {
 
 }
